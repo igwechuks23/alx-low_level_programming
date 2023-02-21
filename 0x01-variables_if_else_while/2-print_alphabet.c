442cfbaec207dc3b6689a718d7a8cd42fe8c706a
igwechuks23 <chukwumamuogbo21@gmail.com>
2023-02-20 20:17:35 -0800

    2. I sometimes suffer from insomnia.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-
 /**
 *main - Entry point
 *Return: Always 0
 */
-
 int main(void)
 {
     int i;
     for (i = 97; i <= 122; i++)
-    {
-        putchar(i);
-    }
-    putchar('\n');
-    return 0;
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
 }
