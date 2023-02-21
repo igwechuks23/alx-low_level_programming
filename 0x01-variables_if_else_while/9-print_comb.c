d60d1d58a54a9c41794548c05600c8aa71fd007a
igwechuks23 <chukwumamuogbo21@gmail.com>
2023-02-20 21:02:24 -0800

    test

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Return: Always 0
+*/
+int main(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = i; j < 10; j++)
+{
+putchar(i + 48);
+putchar(j + 48);
+if (i < 9 || j < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
