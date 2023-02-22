1ec2f895ff61388602b815c7c1b2120a77822d71
igwechuks23 <chukwumamuogbo21@gmail.com>
2023-02-22 15:56:59 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,21 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
-*main - Entry point
-*Return: Always 0
-*/
+ * main - print _putchar
+ * is written by igwechuks23
+ * Return: 0
+ */
+
 int main(void)
 {
-printf("_putchar\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
