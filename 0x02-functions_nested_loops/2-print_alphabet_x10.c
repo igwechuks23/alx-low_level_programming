0c607dd619efab156ce7291894b67a6190eea1b3
igwechuks23 <chukwumamuogbo21@gmail.com>
2023-02-22 16:26:12 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * print_alphabet_x10 - print x10
+ * written by igwechuks23
+ * return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int new_line;
+		for (new_line = 1; new_line <= 10; new_line++)
+	{
+		_putchar("\n");
+		char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter)
+	}
+}
+}
