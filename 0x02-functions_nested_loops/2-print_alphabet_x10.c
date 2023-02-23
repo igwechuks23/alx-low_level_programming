4a8289d11659a5145cd695e70b09e8f02dfe0b9c
igwechuks23 <chukwumamuogbo21@gmail.com>
2023-02-22 16:47:28 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 {
 	int new_line = 0;
 	char letter;
-		while (new_line <= 9; new_line++)
+		while (new_line++ <= 9)
 		{
 			for (letter = 'a'; letter <= 'z'; letter++)
 				_putchar(letter
