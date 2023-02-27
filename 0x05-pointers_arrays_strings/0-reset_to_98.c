01e7afe8c1b553d6a55d7d6f84f18de9257c984a
igwechuks23 <chukwumamuogbo21@gmail.com>
2023-02-27 13:20:46 -0800

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#inculde "main.h"
+
+/**
+ * reser_to_98 -takes the point and updates the value
+ *
+ * 'n: int to check
+ * Owned by Igwechuks23
+ * Return:0 is success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
