75079a4243b54e66018cf815945bc8fcbb4dfe95
igwechuks23 <chukwumamuogbo21@gmail.com>
2023-02-28 13:29:51 -0800

    3. I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,12 +8,12 @@
 
 void _puts(char *str)
 {
-	int i = 0;
+	int a = 0;
 
-	while (str[i] != '\0')
+	while (str[a] != '\0')
 	{
-_putchar(str[i]);	
-		i++;
+		_putchar(str[a]);	
+		a++;
 	}
 	_putchar('\n');
 }
